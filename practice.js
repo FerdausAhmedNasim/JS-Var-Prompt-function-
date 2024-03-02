e4d7d71589caa26d6168d2f5024dbd5c4e066ae3
Ferdaus Ahmed Nasim <ferdausahmed2020@gmail.com>
2024-03-02 23:57:06 +0600

    first commit

---
+++ practice.js
@@ -0,0 +1,70 @@
+var one;
+document.write(one+"<br>")
+
+
+var one= 10;
+var two =5;
+var two=10;
+document.write(two+"<br>");
+
+document.write("2/3"+"<br>");
+
+var add = one +two ;
+
+document.write(add+"<br>");
+var minus=one-two;
+document.write(minus);
+
+
+var divided =one/two;
+document.write(divided+"<br>");
+
+var st="Ferdaus Ahmed Nasim";
+document.write(st+"<br>");
+
+var arr = new Array(1,2,3,4);
+document.write(arr+"<br>");
+
+var bool=true;
+document.write(bool+"<br>");
+
+var floating_point=10.5;
+document.write(floating_point+"<br>");
+
+var obj = new Object('new object' );
+document.write(obj+'<br>');
+
+
+var dt=new Date;
+document.write(dt+'<br>');
+
+var age = prompt("Enter your age");
+alert('your age is:'+age);
+document.write('your age is:'+age+"<br>");
+
+
+console.log("age"+age);
+
+
+// var x=prompt("Enter first Number");
+// var y=prompt("2nd age enter");
+
+// var x=parseFloat(prompt("Enter first Number"));
+// var y=parseFloat(prompt("2nd age enter"));
+// var plus= x + y;
+
+
+// document.write(plus+"<br>");
+
+
+
+// function getTotal() {
+
+
+//     var num1=parseFloat(prompt("Enter first Number:"));
+//     var num2=parseFloat(prompt("2d num"));
+//     var plus= num1 + num2 ;
+//     document.getElementById("showtotal").innerHTML=plus;
+
+
+// }
